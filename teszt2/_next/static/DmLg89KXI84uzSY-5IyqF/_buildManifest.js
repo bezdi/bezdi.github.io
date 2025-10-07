ef26a59ed7f2903e98f9f0b8200824c7d17e47d5
bezdi <zedbangor@gmail.com>
2025-10-07 13:55:49 +0200

    base path config teszt2

---
+++ teszt2/_next/static/DmLg89KXI84uzSY-5IyqF/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/68966b1364ce24e1.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/teszt//_next/:path+",
+        "destination": "/teszt/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ teszt2/_next/static/chunks/1770b09f364035ff.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/0a7be362dcedaaac.js",
+  "static/chunks/turbopack-955fc0e57a29189b.js"
+])
